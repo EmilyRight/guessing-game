678956f27c5a2e78abee0921d2c4d4caccef1de7
EmilyRight <emilya.korkunova@gmail.com>
2021-09-05 20:40:28 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+        this.sugestion;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.sugestion = Math.round((this.max-this.min)/2);
+        return this.sugestion;
     }
 
     lower() {
-
+        this.max = this.sugestion;
     }
 
     greater() {
-
+        this.min = this.sugestion;
     }
 }
 
